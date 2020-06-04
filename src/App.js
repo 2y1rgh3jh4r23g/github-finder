81d63360de14362b25a204966d1d52cd0d5a4ee3
Kirill Anisimov <kirill.anisimov@protonmail.com>
2020-06-04 22:12:37 +0300

    Creates Home component to wrap Search and Users

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
-import Users from "./components/users/Users";
 import User from "./components/users/User";
-import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
+import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 
 import GithubState from "./context/github/GithubState";
@@ -22,22 +21,8 @@ const App = () => {
             <div className='container'>
               <Alert />
               <Switch>
-                {/* Route for home page */}
-                <Route
-                  exact
-                  path='/'
-                  render={(props) => {
-                    return (
-                      <Fragment>
-                        <Search />
-                        <Users />
-                      </Fragment>
-                    );
-                  }}
-                />
-                {/* Route for about page */}
+                <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
-                {/* Route for user page */}
                 <Route exact path='/user/:login' component={User} />
               </Switch>
             </div>
