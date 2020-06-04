af6a0603334820f45678581cd1d064e54600e9dd
Kirill Anisimov <kirill.anisimov@protonmail.com>
2020-06-04 22:05:03 +0300

    Refactors Alert to use Context

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
@@ -8,52 +8,42 @@ import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
 
 import GithubState from "./context/github/GithubState";
+import AlertState from "./context/alert/AlertState";
 
 import "./App.css";
 
 const App = () => {
-  const [alert, setAlert] = useState(null);
-
-  // Set Alert
-  const showAlert = (msg, type) => {
-    setAlert({
-      msg,
-      type,
-    });
-
-    // make the alert disappear after 5 seconds
-    setTimeout(() => setAlert(null), 5000);
-  };
-
   return (
     <GithubState>
-      <BrowserRouter>
-        <div className='App'>
-          <Navbar />
-          <div className='container'>
-            <Alert alert={alert} />
-            <Switch>
-              {/* Route for home page */}
-              <Route
-                exact
-                path='/'
-                render={(props) => {
-                  return (
-                    <Fragment>
-                      <Search setAlert={showAlert} />
-                      <Users />
-                    </Fragment>
-                  );
-                }}
-              />
-              {/* Route for about page */}
-              <Route exact path='/about' component={About} />
-              {/* Route for user page */}
-              <Route exact path='/user/:login' component={User} />
-            </Switch>
+      <AlertState>
+        <BrowserRouter>
+          <div className='App'>
+            <Navbar />
+            <div className='container'>
+              <Alert />
+              <Switch>
+                {/* Route for home page */}
+                <Route
+                  exact
+                  path='/'
+                  render={(props) => {
+                    return (
+                      <Fragment>
+                        <Search />
+                        <Users />
+                      </Fragment>
+                    );
+                  }}
+                />
+                {/* Route for about page */}
+                <Route exact path='/about' component={About} />
+                {/* Route for user page */}
+                <Route exact path='/user/:login' component={User} />
+              </Switch>
+            </div>
           </div>
-        </div>
-      </BrowserRouter>
+        </BrowserRouter>
+      </AlertState>
     </GithubState>
   );
 };
